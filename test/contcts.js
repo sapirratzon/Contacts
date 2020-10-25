58efc2d7aadc1abe609701636ecb02789718381e
sapirratzon <sapir.ratzon23@gmail.com>
2020-10-25 13:41:09 +0200

    Add test: route for getting all contacts.

--- test/contcts.js
+++ test/contcts.js
@@ -1,5 +1,6 @@
 var app = require("../app");
 var request = require("supertest")(app);
+
 var expect = require("expectacle");
 
 describe("the whose-there", function () {
@@ -12,6 +13,15 @@ describe("the whose-there", function () {
                 expect(result.text).toBe("Hi Trax! This is SAPIR RATZON");
                 done();
             });
+    });
+});
+
+
+describe("the getAllContacts", function () {
+    it("should return all the contacts", function(done) {
+        request.post("/contacts/getAllContacts")
+            .expect(200, [{id: '1', name: 'Sapir Ratzon', phone: '0546680530', country: 'Israel', city: 'Ramat Gan'},
+            {id: '2', name: 'Yuval Lasri', phone: '0503953901', country: 'Israel', city: 'Ramat Gan'}], done);
     });
 });
 
